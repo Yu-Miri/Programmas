3a18c57d7a80f3caabf8a258b859d0cfe0814d98
Yu-Miri <121469490+Yu-Miri@users.noreply.github.com>
2023-05-07 16:12:41 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -15,14 +15,14 @@ def solution(N, stages):
 #         # 플레이어 수만큼 돌면서 중복 제거 전의 실패율 리스트(f_ratio)에서 실패율(i)이 중복으로 존재하는 값을 전부 찾아서 리스트에 index를 더하기
 #     return [rank + 1 for rank in fail_stage]
 
-    fail_stage = {}
-    ls = len(stages) # 스테이지에 도달한 플레이어 수
+    fail_ratio = {}
+    players = len(stages) # 스테이지에 도달한 플레이어 수
 
     for i in range(1,N+1): # 스테이지 번호 for문
-        if ls != 0:
-            fail_stage[i] = stages.count(i)/ls  # 각각의 스테이지와 실패율을 계산해서 딕셔너리에 추가
-            ls -= stages.count(i) # 다음 스테이지에 대해서 실패한 플레이어 수 빼기
-        else:
-            fail_stage[i] = 0
-    return sorted(fail_stage, key=lambda i: fail_stage[i], reverse=True)
+        if players != 0: # ls가 0인 경우에
+            fail_ratio[i] = stages.count(i) / players  # 각각의 스테이지와 실패율을 계산해서 딕셔너리에 추가
+            players -= stages.count(i) # 다음 스테이지에 대해서 실패한 플레이어 수 빼기
+        else: #
+            fail_ratio[i] = 0
+    return sorted(fail_ratio, key=lambda i: fail_ratio[i], reverse=True)
     #fail_stage의 value값을 기준으로 내림차순 정렬해서 key값을 return

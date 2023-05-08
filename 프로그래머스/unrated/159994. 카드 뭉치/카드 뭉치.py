e7634c19093e578c8f11d2aeb29b346b2a46861f
Yu-Miri <121469490+Yu-Miri@users.noreply.github.com>
2023-05-08 12:15:13 +0900

    Update 카드 뭉치.py

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,6 +1,4 @@
 def solution(cards1, cards2, goal):
-    # answer = []
-    # for i in goal: # 목표 문장 for문
     #     try:
     #         if i == cards1[0]: # 카드뭉치 1 첫 번째 카드가 i와 같다면 
     #             cards1.pop(0) # 맨 앞에 카드 제거
@@ -12,12 +10,12 @@ def solution(cards1, cards2, goal):
     #         else: # 같지 않다면
     #             return "No" # No return
     # return "Yes" # 전부 통과하면 Yes return
-    # answer = []
+
     for i in goal: # 목표 문장 for문
-        if len(cards1) and i == cards1[0]:
-            cards1.pop(0)
+        if len(cards1) and i == cards1[0]: # 카드뭉치에 카드가 남아있고 첫 번째 카드가 목표 단어와 같다면
+            cards1.pop(0) # 맨 앞 카드를 제거
         elif len(cards2) and i == cards2[0]:
             cards2.pop(0)
-        else:
-            return "No"
-    return "Yes"
+        else: # 문장을 만들 수 없다면
+            return "No" # "No" 반환
+    return "Yes" # 전부 통과했다면 "Yes" 반환

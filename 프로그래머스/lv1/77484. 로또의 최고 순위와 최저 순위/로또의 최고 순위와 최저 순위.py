7ea83d883c695ffecb51869b5e77ec7db8de5ed9
Yu-Miri <121469490+Yu-Miri@users.noreply.github.com>
2023-05-11 13:10:11 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,20 +1,28 @@
+# def solution(lottos, win_nums):
+#     rank = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6} # 맞춘 개수 : 순위 배열
+#     collect = 0 # 지워지지 않은 개수에서 맞춘 개수
+#     plus = lottos.count(0) #지워진 번호의 개수
+
+#     for lotto in lottos:
+#         if lotto in win_nums: # 하나의 번호가 정답 로또에 있다면
+#             collect += 1 # 맞은 개수를 더하기
+
+#     max_score = rank[collect+plus] # 지워진 번호의 개수를 더해서 최고 점수 계산
+#     min_score = rank[collect]
+#     if plus == 6:
+#         return [1, 6]
+#     elif plus == 0: # 지워진 번호가 없다면
+#         return [min_score, min_score]
+#     elif 0 < plus < 6:
+#         return [max_score, min_score]
+    
+#---맞추고 참고---
 def solution(lottos, win_nums):
-    rank = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6} # 맞춘 개수 : 순위 배열
-    collect = 0 # 지워지지 않은 개수에서 맞춘 개수
-    plus = lottos.count(0) #지워진 번호의 개수
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    cnt_0 = lottos.count(0) # 지워진 번호의 개수를 저장
+    collect = 0
 
-    for lotto in lottos:
-        if lotto in win_nums: # 하나의 번호가 정답 로또에 있다면
-            collect += 1 # 맞은 개수를 더하기
-# 경우의 수
-# 전부 알고 있는 경우 collect만 return (plus가 0인 경우)
-# 전부 모르는 경우 1, 6을 return (plus가 6인 경우)
-
-    max_score = rank[collect+plus]
-    min_score = rank[collect]
-    if plus == 6:
-        return [1, 6]
-    elif plus == 0: # 지워진 번호가 없다면
-        return [min_score, min_score]
-    elif 0 < plus < 6:
-        return [max_score, min_score]
+    for num in lottos: # 체크한 번호를 for문
+        if num in win_nums: # 정답 번호에 있다면
+            collect += 1 # 맞춘 개수에 더하고
+    return rank[cnt_0 + collect], rank[collect] # 맞춘 개수에 지워진 개수와 맞춘 개수를 return

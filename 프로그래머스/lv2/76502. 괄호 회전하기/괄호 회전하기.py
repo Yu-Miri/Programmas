c31ec6fc34ed19521f514fc3ffac498af862ded2
Yu-Miri <121469490+Yu-Miri@users.noreply.github.com>
2023-05-22 22:22:50 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,21 @@
+def solution(s):
+    answer = 0
+    chk_list = [s]
+    for i in range(len(s)-1):
+        s = s[1:]+s[0]
+        chk_list.append(s)
+    
+    for str in chk_list:
+        stack = []
+        for i in str:
+            if len(stack) == 0:
+                stack.append(i)
+            else:
+                if i == ')' and stack[-1] == '(':   stack.pop()
+                elif i == ']' and stack[-1] == '[':   stack.pop()
+                elif i == '}' and stack[-1] == '{':   stack.pop()
+                else: stack.append(i)
+        if len(stack) == 0:
+            answer += 1
+        
+    return answer
